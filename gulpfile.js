06462a085a8ee4d2c6072137dcfef77027eee3b6
Nathan Tiedt <decafchild@gmail.com>
2019-09-25 14:42:59 -0600

    started package

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
-const { src, dest } = require('gulp');
-const replace = require('gulp-replace');
+const { src, dest, parallel } = require('gulp');
+const { join, resolve } = require('path');
+const fs = require('fs');
+const install = require('gulp-install');
 const program = require('commander');
+const replace = require('gulp-replace');
+const tsProject = require('gulp-typescript');
 
 const NO_NAME = 'NONE';
 
-function startNewLambda() {
+function startLambdaFunction() {
   program
     .option('-n, --name <lambda-name>', 'Name of Lambda Function', NO_NAME)
     .option('-d, --desc <description>', 'Description of Lambda', '');
   program.parse(process.argv);
-  if (program.name === NO_NAME)
+  if (program.name === 'NONE')
     throw new Error('No Name Given');
   return src('./.template/*')
     .pipe(replace('lambda_function_name', program.name))
@@ -17,4 +21,27 @@ function startNewLambda() {
     .pipe(dest(`./${program.name}/`));
 }
 
-exports.create = startNewLambda;
+function packageFunction(lambda) {
+  return Promise.resolve(
+    src(`./${lambda}/`)
+      .pipe(tsProject())
+      .pipe(dest(`./build/${lambda}`))
+  );
+}
+
+function packageLambdas(done) {
+  let lambdas = Promise.all(
+    fs.readdirSync('./')
+      .filter( (dir) => {
+        return dir !== '.template'
+            && fs.existsSync(join(dir, 'package.json'));
+      })
+      .map( (lambda) => {
+        return packageFunction(lambda);
+      })
+  );
+  return lambdas;
+}
+
+exports.create = startLambdaFunction;
+exports.package = packageLambdas;
