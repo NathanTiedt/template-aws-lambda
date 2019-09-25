5c9772798abb862c7f8e598c14c1b7c19add8a56
Nathan Tiedt <decafchild@gmail.com>
2019-09-25 12:03:45 -0600

    added commander

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,16 @@
 const { src, dest } = require('gulp');
-
-function parseArgs() {
-  let args = {};
-  const argv = process.argv;
-  for(let i=2; i < argv.length; i++) {
-    if (argv[i].startsWith('--') 
-        && !argv[i+1].startsWith('--')) {
-      args[argv[i].replace(/\-/g, '')] = argv[i+1];
-    }
-  }
-  return args;
-}
+const replace = require('gulp-replace');
+const program = require('commander');
 
 function startNewLambda() {
-  const args = parseArgs();
+  program
+    .option('-n, --name <lambda-name>', 'Name of Lambda Function')
+    .option('-d, --desc <description>', 'Description of Lambda', '');
+  program.parse(process.argv);
   return src('./.template/*')
-    .pipe(dest(`./${args.name}/`));
+    .pipe(replace('lambda_function_name', program.name))
+    .pipe(replace('lambda_function_description', program.desc))
+    .pipe(dest(`./${program.name}/`));
 }
 
 exports.create = startNewLambda;
